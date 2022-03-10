893868802c33923fcfe992d94afc0086ef83170a
Marcel Reig <marcelreig@gmail.com>
2022-03-10 20:49:54 +0100

    put together fetchPostsWithQuery action to receive queried posts

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,4 @@
-import { SET_RECENT_POSTS } from './types';
+import { SET_RECENT_POSTS } from "./types";
 
 import axios from "axios";
 
@@ -8,8 +8,22 @@ export function fetchRecentPosts() {
       console.log(response.data.posts);
       dispatch({
         type: SET_RECENT_POSTS,
-        payload: response.data.posts
-      })
+        payload: response.data.posts,
+      });
     });
+  };
+}
+
+export function fetchPostsWithQuery(query) {
+  return function (dispatch) {
+    axios
+      .get(`https://api.dailysmarty.com/search?q=${query}`)
+      .then((response) => {
+        console.log(response.data.posts);
+        // dispatch({
+        //   type: SET_RECENT_POSTS,
+        //   payload: response.data.posts,
+        // });
+      });
   };
 }
--- src/components/home.js
+++ src/components/home.js
@@ -4,21 +4,26 @@ import Logo from "./logo";
 import SearchBar from "./searchBar";
 import RecentPosts from "./recentPosts";
 
-export default class Home extends Component {
-  handleFormSubmit(query) {
-    console.log("trying to handle submit for query", query);
+import { connect } from "react-redux";
+import * as actions from "../actions";
+
+class Home extends Component {
+  handleSearchBarSubmit(query) {
+    this.props.fetchPostsWithQuery(query);
     this.props.history.push("/results");
-  };
+  }
 
   render() {
     return (
       <div>
         <div>
           <Logo />
-          <SearchBar onSubmit={(query) => this.handleFormSubmit(query)} />
+          <SearchBar onSubmit={(query) => this.handleSearchBarSubmit(query)} />
           <RecentPosts />
         </div>
       </div>
     );
   }
 }
+
+export default connect(null, actions)(Home);
