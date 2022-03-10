8cf836a0d6ebaafc022b31b67d86ef8fed20e064
Marcel Reig <marcelreig@gmail.com>
2022-03-10 19:18:19 +0100

    modified SearchBar Component

--- src/components/home.js
+++ src/components/home.js
@@ -5,12 +5,17 @@ import SearchBar from "./searchBar";
 import RecentPosts from "./recentPosts";
 
 export default class Home extends Component {
+  handleFormSubmit(query) {
+    console.log("trying to handle submit for query", query);
+    this.props.history.push("/results");
+  };
+
   render() {
     return (
       <div>
         <div>
           <Logo />
-          <SearchBar />
+          <SearchBar onSubmit={(query) => this.handleFormSubmit(query)} />
           <RecentPosts />
         </div>
       </div>
