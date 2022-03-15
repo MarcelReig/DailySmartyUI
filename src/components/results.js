74ec85deea0f4ae84a5233e760430f7ab67826ed
Marcel Reig <marcelreig@gmail.com>
2022-03-15 20:04:50 +0100

    styled results component

--- src/components/results.js
+++ src/components/results.js
@@ -3,8 +3,8 @@ import Logo from "./logo";
 import SearchBar from "./searchBar";
 import ResultsPosts from "./resultsPosts";
 
-import { connect } from 'react-redux';
-import * as actions from '../actions';
+import { connect } from "react-redux";
+import * as actions from "../actions";
 
 class Results extends Component {
   handleSearchBarSubmit(query) {
@@ -13,7 +13,7 @@ class Results extends Component {
 
   render() {
     return (
-      <div>
+      <div className="results">
         <Logo size={55} />
         <SearchBar onSubmit={(query) => this.handleSearchBarSubmit(query)} />
         <ResultsPosts />
